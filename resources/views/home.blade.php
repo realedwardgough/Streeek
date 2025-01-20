a59730677b8594a5f7a641a990968319bd049114
Edward Gough <edwardggough@gmail.com>
2025-01-20 19:05:16 +0000

    Base HTML structure and inital styles.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Streeek: Easy Habit Manager</title>
+    <link rel="stylesheet" href="{{ asset(path: 'css/app.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+
+    <!-- Axios CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script> 
+
+</head>
+<body>
+    
+    <nav>
+        <header>
+            <h1><b>Streeek</b>.</h1>
+            <h2>Get your streeek on!</h2>
+        </header>
+    </nav>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+/**
+ * Import necessary controllers and models for route definitions.
+ */
 use Illuminate\Support\Facades\Route;
 
+
+/**
+ * Web Routes for the Application
+ */
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
